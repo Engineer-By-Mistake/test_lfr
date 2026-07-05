7e63bc4dabd7bc7b84f322d2567fc63e1160ba60
Rudra.Thakar <Rudra7thakar@gmail.com>
2026-07-05 23:28:34 +0530

    On branch main

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -106,20 +106,15 @@ int main(void)
   MX_I2C1_Init();
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
-  MPU6050_Init(); 
-  motor_pin_set(&htim1);
-  /* USER motor_startCODE END 2 */
+
+  /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
     /* USER CODE END WHILE */
-    motor_control(500, 500, &htim1);
-    HAL_Delay(500);
-    motor_control(0, 0, &htim1);
-    HAL_Delay(500);
-    motor_control(-500, -500, &htim1);
+
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -393,6 +388,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA14 PA15 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14|GPIO_PIN_15;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
